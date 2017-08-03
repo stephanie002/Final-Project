0b81702db49775df6bf2000d211a52bad2430bd2
jezzzae <jessicvences65@gmail.com>
2017-08-03 09:58:33 -0500

    First commit

---
+++ blog/migrations/__init__.py
@@ -0,0 +1,4 @@
+(myvenv) ~/djangogirls$ python manage.py makemigrations blog
+Migrations for 'blog':
+  blog/migrations/0001_initial.py:
+  - Create model Post
--- blog/models.py
+++ blog/models.py
@@ -2,15 +2,19 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
+class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    phone_number = models.CharField(max_length=12)
+    email = models.EmailField(max_length=60)
 
-    
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+class EmergencyContact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    email = models.Charfield(max_length=80)
+    custom_message = models.CharField(max_length=80)
+    self.published_date = timezone.now()
+    self.save()
 
     def __str__(self):
         return self.title
