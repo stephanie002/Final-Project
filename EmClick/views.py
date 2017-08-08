47ef820acb0b18df70f3fbbbb1aa4e781b957b02
jezzzae <jessicvences65@gmail.com>
2017-08-08 13:28:09 -0500

    ss

--- EmClick/views.py
+++ EmClick/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import EmergencyContact
+
 
 
 def account(request):
-    return render(request, 'account.html',{})
-    
+    contacts=EmergencyContact.objects.filter(user=request.user)
+    return render(request, 'account.html',{"contacts":contacts})
+
 
 # Create your views here.
